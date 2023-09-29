4a46a3c03e79afb7d75fbb74d71807cc9a749372
Manojdeore7 <manojddeore11>
2023-09-29 19:08:07 +0530

    getstaticpaths is done

--- components/meetups/DetailedPage.js
+++ components/meetups/DetailedPage.js
@@ -3,9 +3,10 @@ import classes from "./DetailedPage.module.css";
 export default function DetailedPage(props) {
   return (
     <section className={classes.detail}>
-      <img src={props.img} />
-      <h1>{props.title}</h1>
-      <h3>{props.address}</h3>
+      <img src={props.meetup.img} />
+      <h1>{props.meetup.title}</h1>
+      <h3>{props.meetup.address}</h3>
+      <h3>{props.meetup.id}</h3>
     </section>
   );
 }
--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,11 +1,37 @@
 import DetailedPage from "../../components/meetups/DetailedPage";
-
-export default function meetupid() {
-  return (
-    <DetailedPage
-      img="https://images.unsplash.com/photo-1587325889132-2c9e393e7d70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dG93bnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
-      title="hi"
-      address="hi"
-    />
-  );
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupid: "1",
+        },
+      },
+      {
+        params: {
+          meetupid: "2",
+        },
+      },
+    ],
+  };
 }
+function meetupid(props) {
+  return <DetailedPage meetup={props.meetup} />;
+}
+export async function getStaticProps(context) {
+  //apply for async API
+  console.log(context);
+  let meetupID = context.params.meetupid;
+  return {
+    props: {
+      meetup: {
+        img: "https://images.unsplash.com/photo-1587325889132-2c9e393e7d70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dG93bnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+        title: "hi",
+        address: "hi",
+        id: meetupID,
+      },
+    },
+  };
+}
+export default meetupid;
